c4963d40c95339bf032cafa3a442a3f0d96035c3
koki murakami <koki.m3626@gmail.com>
2020-06-05 15:01:47 +0000

    Laravel14

--- ProfileController.php
+++ ProfileController.php
@@ -5,21 +5,44 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Profile;
+
 class ProfileController extends Controller
 {
     public function add(){
         return view('admin.profile.create');
     }
     
-    public function create(){
+    public function create(Request $request)
+    {
+        $this->validate($request, Profile::$rules);
+        
+        $news = new Profile;
+        $form = $request->all();
+        
+        unset($form['_token']);
+        
+        $news->fill($form);
+        $news->save();
+
+
         return redirect('admin/profile/create');
     }
     
-    public function edit(){
-        return view('admin.profile.edit');
+    public function edit(Request $request){
+        $profile = Profile::find($request->id);
+        if (empty($profile)) {
+        abort(404);    
+      }
+        return view('admin.profile.edit',['profile_form' => $profile]);
     }
     
-    public function update(){
+    public function update(Request $request){
+        $this->validate($request, Profile::$rules);
+        $profile = Profile::find($request->id);
+        $profile_form = $request->all();
+        unset($profile_form['_token']);
+        $profile->fill($profile_form)->save();
         return redirect('admin/profile/edit');
     }
 }
