d143ca5e4be17945123ea16da90c0efc9a2de747
koki murakami <koki.m3626@gmail.com>
2020-06-09 09:23:46 +0000

    var2

--- ProfileController.php
+++ ProfileController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 use App\Profile;
+use App\ProfileHistory;
+use Carbon\Carbon;
+
 
 class ProfileController extends Controller
 {
@@ -17,32 +20,53 @@ class ProfileController extends Controller
     {
         $this->validate($request, Profile::$rules);
         
-        $news = new Profile;
+        $profile = new Profile;
         $form = $request->all();
         
         unset($form['_token']);
         
-        $news->fill($form);
-        $news->save();
+        $profile->fill($form);
+        $profile->save();
 
 
         return redirect('admin/profile/create');
     }
     
+    public function index(Request $request)
+  {
+      $cond_title = $request->cond_title;
+      if ($cond_title != '') {
+          $posts = Profile::where('title', $cond_title)->get();
+      } else {
+          $posts = Profile::all();
+      }
+      return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+  }
+
+
+    
     public function edit(Request $request){
         $profile = Profile::find($request->id);
         if (empty($profile)) {
-        abort(404);    
+        abort(404);
       }
-        return view('admin.profile.edit',['profile_form' => $profile]);
+        return view('admin.profile.edit', ['profile_form' => $profile]);
     }
     
-    public function update(Request $request){
+    public function update(Request $request)
+    {
         $this->validate($request, Profile::$rules);
         $profile = Profile::find($request->id);
         $profile_form = $request->all();
+        // dd($profile);
         unset($profile_form['_token']);
         $profile->fill($profile_form)->save();
-        return redirect('admin/profile/edit');
+       
+        $history = new ProfileHistory;
+        $history->profile_id = $profile->id;
+        $history->edited_at = Carbon::now();
+        $history->save();
+        
+        return redirect('admin/profile');
     }
 }
